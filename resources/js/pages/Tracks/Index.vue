ce7c9a08c88311dc7ccaa3d6de4949cc4bf65d60
Antoine Durussel <55091110+AntoineDrsl@users.noreply.github.com>
2025-12-08 16:53:29 +0100

    Day 1

---
+++ resources/js/pages/Tracks/Index.vue
@@ -0,0 +1,37 @@
+<template>
+    <MusicLayout>
+        <template #title>
+            Liste des musiques
+        </template>
+
+        <template #actions>
+
+        </template>
+
+        <template #content>
+            <div class="grid grid-cols-4 gap-4">
+                <Track
+                    v-for="track in tracks"
+                    :key="track.slug"
+                    :track="track"
+                />
+            </div>
+        </template>
+    </MusicLayout>
+</template>
+
+<script>
+    import MusicLayout from '@/layouts/MusicLayout.vue';
+    import Track from '@/components/Tracks/Track.vue';
+
+    export default {
+        name: 'Index',
+        components: {
+            MusicLayout,
+            Track,
+        },
+        props: {
+            tracks: Array,
+        }
+    }
+</script>
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TrackController;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Laravel\Fortify\Features;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canRegister' => Features::enabled(Features::registration()),
+    ]);
+})->name('home');
+
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/settings.php';
+
+Route::get('test', [HomeController::class, 'test'])->name('test');
+
+Route::get('tracks', [TrackController::class, 'index'])->name('tracks.index');
